2e94108811f22d7ef07c7f5fde7df8c88ab75e10
khalidfsh <dev.khalid@me.com>
2019-03-26 20:38:39 +0300

    Add multiparty consensus unit test

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -164,47 +164,121 @@ contract('Flight Surety Tests', async (accounts) => {
 
       await TruffleAssert.passes(
         config.flightSuretyData.getAirline(config.owner),
+        "cannot retrive existed airline"
       );
     });
 
     it(`registered airline can funds contract with minemum of 10 ether, more than that will be returnd to funder`, async() => {
       let airlineBalanceBefore = new BigNumber(await web3.eth.getBalance(config.airlinesByMediation[0]))
-      let fundingValue = new BigNumber(web3.utils.toWei('20', "ether"))
+      let dataContractBalanceBefore = new BigNumber(await web3.eth.getBalance(config.flightSuretyData.address))
+      let fundingValue = new BigNumber(web3.utils.toWei('10', "ether"))
+
       await TruffleAssert.reverts(
         config.flightSuretyApp.fundMyAirline({
           from: config.airlinesByMediation[0], 
           value: web3.utils.toWei('9.9', "ether")
         }),
-        "Funding must be 10 Ether"
+        "Funding must be 10 Ether",
+        "Funding less than 10 Ether accepted"
       );
       await TruffleAssert.passes(
         config.flightSuretyApp.fundMyAirline({
           from: config.airlinesByMediation[0], 
           value: fundingValue
         }),
+        "funndig proccess did not passes"
       );
 
       let airlineBalanceAfter = new BigNumber(await web3.eth.getBalance(config.airlinesByMediation[0]))
-      assert(airlineBalanceBefore.isGreaterThan(airlineBalanceAfter))
-      assert(airlineBalanceAfter.isGreaterThan(airlineBalanceBefore.minus(fundingValue)))
+      let dataContractBalanceAfter = new BigNumber(await web3.eth.getBalance(config.flightSuretyData.address))
+
+      assert(airlineBalanceBefore.isGreaterThan(airlineBalanceAfter), "balance of funder did not change")
+      assert(dataContractBalanceAfter.isGreaterThan(dataContractBalanceBefore), "balance of data contract did not change")
+    });
+
+    it(`airline will be active after success funding proccess`, async() => {
+      assert(await config.flightSuretyApp.isAirlineFunded.call(config.airlinesByMediation[0]), 'not isAirlineFunded')
+    });
+
+    it(`new funds addesd to airline balance of funds`, async() => {
+
     });
     
   });
 
 
 
-  //TODO
   describe(`\n💁🏾‍♂️👨🏼‍⚖️🙅🏻‍♂️ Multiparty 💁🏾‍♂️👨🏼‍⚖️🙅🏻‍♂️:`, async() => {
-    it(``, async() => {
-
+    it(`deployed with type of mediation registration`, async() => {
+      /// RegisterationType.BY_MEDIATION == 0
+      assert(await config.flightSuretyData.getRegistrationType.call(), '0', "registration type not BY_MEDIATION");
     });
 
-    it(``, async() => {
+    it(`only active airline (funded) can register new airline using`, async() => {
+      await TruffleAssert.passes(
+        config.flightSuretyApp.registerAirline(config.airlinesByMediation[1], 'TestAirline2', {from: config.airlinesByMediation[0]}),
+      );
 
+      let addedAirline = await config.flightSuretyData.getAirline.call(config.airlinesByMediation[1]);
+      assert.equal(addedAirline.name, 'TestAirline2', "airline dose not registered by app contract (registerAirline)");
     });
 
-    it(``, async() => {
+    it(`first 4 registered airline added by mediation`, async() => {
+      await TruffleAssert.passes(
+        config.flightSuretyApp.registerAirline(config.airlinesByMediation[2], 'TestAirline3', {from: config.owner}),
+      );
+    });
 
+    it(`registration type will be by votes after 4th registration`, async() => {
+      /// RegisterationType.BY_VOTES == 1
+      assert(await config.flightSuretyData.getRegistrationType.call(), '1', "registration type not BY_MEDIATION");
+    });
+
+    it(`the 5th register airline will be in (waiting for votes) state`, async() => {
+      await TruffleAssert.passes(
+        config.flightSuretyApp.registerAirline(config.airlinesByVotes[0], 'TestAirline4', {from: config.airlinesByVotes[0]}),
+      );
+
+      let addedAirline = await config.flightSuretyData.getAirline.call(config.airlinesByVotes[0]);
+      //AirlineRegisterationState.WaitingForVotes == 0
+      assert.equal(addedAirline.state, '0', "airline state not waiting for votes");
+    });
+
+    it(`only active (funded) airline can votes `, async() => {
+      //adding extra fundded airline to make them 3 active airline (was 2 before)
+      await config.flightSuretyApp.fundMyAirline({
+        from: config.airlinesByMediation[1], 
+        value: web3.utils.toWei('10', "ether")
+      });
+
+      await TruffleAssert.passes(
+        config.flightSuretyApp.voteForAirline(config.airlinesByVotes[0], {from: config.owner}),
+      );
+
+      let addedAirline = await config.flightSuretyData.getAirline.call(config.airlinesByVotes[0]);
+      assert.equal(addedAirline.numberOfVotes, '1', "votes did not change")
+      assert.equal(addedAirline.state, '0', "state changed before 50% consensus")
+    });
+
+    it(`airline in waiting for votes state should not change its state untill 50% of active airline votes`, async() => {
+      await TruffleAssert.passes(
+        config.flightSuretyApp.voteForAirline(config.airlinesByVotes[0], {from: config.airlinesByMediation[1]}),
+      );
+
+      let addedAirline = await config.flightSuretyData.getAirline.call(config.airlinesByVotes[0]);
+      assert.equal(addedAirline.numberOfVotes, '2')
+      //AirlineRegisterationState.registered == 1
+      assert.equal(addedAirline.state, '1')
+    });
+
+    it(`voting for non waiting for votes airline will fail using requireIsAirlineWaitingForVotes`, async() => {
+      await TruffleAssert.reverts(
+        config.flightSuretyApp.voteForAirline(config.airlinesByVotes[0], {
+          from: config.airlinesByMediation[0], 
+        }),
+        "Airline not waiting for votes",
+        "Airline is waiting for votes!!"
+      );
     });
 
   });
